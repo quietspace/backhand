a0706a566ed75a09c96fa1a183fb08c1b6f86792
Forkk <forkk@forkk.net>
2015-06-03 22:12:48 -0500

    Add dev-shell and release.nix files for Nix
    This will allow backhand to be built using the Nix package manager and
    the related Hydra CI system. Furthermore, the dev-shell script allows
    Nix to construct a dedicated environment for development with all of
    Backhand's dependencies installed.

---
+++ release.nix
@@ -0,0 +1,26 @@
+{ backhandSrc ? { outPath = ./.; revCount = 0; shortRev = "abcdef"; rev = "HEAD"; }
+, officialRelease ? false
+}:
+
+let
+  backhand = h: with import <nixpkgs> {}; with h; mkDerivation {
+    pname = "backhand";
+    version = "0.0.0.0";
+    src = backhandSrc;
+    isLibrary = false;
+    isExecutable = true;
+    buildDepends = [
+      base cabal-install
+      async stm stm-chans mtl transformers lifted-base resourcet containers
+      unordered-containers ixset time text bytestring aeson websockets
+      reactive-banana
+    ];
+    description = "Back-end connection management system for Internet card games";
+    license = stdenv.lib.licenses.mit;
+  };
+in rec {
+  build = {
+    ghc7-10-1 = with import <nixpkgs> {}; backhand haskell.packages.ghc7101;
+    ghc7-8-4 = with import <nixpkgs> {}; backhand haskell.packages.ghc784;
+  };
+}
